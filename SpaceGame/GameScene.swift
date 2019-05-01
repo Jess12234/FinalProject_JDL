c0c51cc29d62bde735d187ab2b966e56dc50989d
Jessica Lowry <jdl14a@dmc-207-04.local>
2019-05-01 08:46:51 -0500

    Falling enemies (no collision yet)

--- SpaceGame/GameScene.swift
+++ SpaceGame/GameScene.swift
@@ -23,7 +23,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     var gametimer:Timer!
-    var possibleAliens = ["alien1", "alien2", "alien3"]
+    var possibleAliens = ["alien", "alien2", "alien3"]
+    
+    let alienCategory:UInt32 = 0x1 << 1
+    let photonTorpedoCategory:UInt32 = 0x1 << 0
+    
     
     override func didMove(to view: SKView)
     {
@@ -62,8 +66,30 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         let alien = SKSpriteNode(imageNamed: possibleAliens[0])
         
-        let randomAlienPosition = GKRandomDistribution(lowestValue: 0, hiestValue: 414)
+        let randomAlienPosition = GKRandomDistribution(lowestValue: 0, highestValue: 414)
+        let position = CGFloat(randomAlienPosition.nextInt())
         
+        alien.position = CGPoint(x: position - 300, y: 550)
+        
+        alien.physicsBody = SKPhysicsBody(rectangleOf: alien.size)
+        alien.physicsBody?.isDynamic = true
+        
+        alien.physicsBody?.categoryBitMask = alienCategory
+        alien.physicsBody?.contactTestBitMask = photonTorpedoCategory
+        alien.physicsBody?.collisionBitMask = 0
+        
+        self.addChild(alien)
+        
+        let animationDuration:TimeInterval = 10
+        
+        
+        var actionArray = [SKAction]()
+        
+        actionArray.append(SKAction.move(to: CGPoint(x: position - 300 , y: -600), duration: animationDuration))
+        
+        actionArray.append(SKAction.removeFromParent())
+        
+        alien.run(SKAction.sequence(actionArray))
     }
     
     
